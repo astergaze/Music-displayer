d39e9be74330fe83300f13dd16bb60460990aeac
hoaxed <thiagomartinez960@gmail.com>
2025-10-30 10:54:40 -0300

    Mouse animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
 import './css/App.css'
 import logo from './assets/logo-azul.png'
+import mouseIcon from "./assets/Mouse-icon.svg" 
 import Footer from './Footer.tsx'
+
 function App() {
 
   return (
@@ -12,8 +13,14 @@ function App() {
           <img src={logo} alt="" />
           <h1>Proyectos creados por estudiantes</h1>
           <p>Musica creada por IA, con fines educativos sobre temas de programacion en C#</p>
+          <img 
+            src={mouseIcon} 
+            alt="Deslizar abajo" 
+            className='mouse-animation' 
+          />
         </div>
       </section>
+      
       <section className='musicCardsComponents'>
           <h2>Repositorio de canciones</h2>
           <div className='musicCardsCont'>
