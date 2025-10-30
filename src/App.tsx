2c6a3d8ab245e8a0ab75f8019d6aae554e2771cf
hoaxed <thiagomartinez960@gmail.com>
2025-10-30 10:35:49 -0300

    Main structure done

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './css/App.css'
+import logo from './assets/logo-azul.png'
+import Footer from './Footer.tsx'
+function App() {
+
+  return (
+    <>
+    <main>
+      <section>
+        <div className='landingFirst'>
+          <img src={logo} alt="" />
+          <h1>Proyectos creados por estudiantes</h1>
+          <p>Musica creada por IA, con fines educativos sobre temas de programacion en C#</p>
+        </div>
+      </section>
+      <section className='musicCardsComponents'>
+          <h2>Repositorio de canciones</h2>
+          <div className='musicCardsCont'>
+          </div>
+      </section>
+    </main>
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+import "./css/Footer.css"
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>Escuela Técnica N°26 "Confederacion Suiza"</p>
+        <p>Proyecto de Generación Musical con IA</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
